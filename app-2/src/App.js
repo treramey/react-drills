ef9a4a4d9f6177646c6e5aed861a3e033b223626
Trevor Ramey <treramey@gmail.com>
2020-03-10 14:16:34 -0500

    Q2 finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      arr: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+  render() {
+    let food = this.state.arr.map((el, i) => {
+      return <h2 key={i}>{el}</h2>;
+    });
+    return (
+      <div className="App">
+        <input type="text" />
+        <h2>{food}</h2>
+      </div>
+    );
+  }
 }
-
-export default App;
